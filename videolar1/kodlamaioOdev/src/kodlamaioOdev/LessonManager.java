aca68dd633880d6f5bbca7781f6652205d049c10
Oğuzhan Belli <75687376+oguzhanbelli@users.noreply.github.com>
2021-04-27 21:55:14 +0300

    Update LessonManager.java

--- videolar1/kodlamaioOdev/src/kodlamaioOdev/LessonManager.java
+++ videolar1/kodlamaioOdev/src/kodlamaioOdev/LessonManager.java
@@ -5,13 +5,13 @@ public class LessonManager {
 		this.lessonId = lessonId;
 		this.lessonTitle = lessonTitle;
 		
-		System.out.println(lessonId+" Numaralý "+lessonTitle+" "+"Programına Dahil Oldunuz.");
+		System.out.println(lessonId+" Numaralı "+lessonTitle+" "+"Programına Dahil Oldunuz.");
 	}
 	public void quitTheProgram(int lessonId,String lessonTitle) {
 		this.lessonId = lessonId;
 		this.lessonTitle = lessonTitle;
 		
-		System.out.println(lessonId+" Numaralý "+lessonTitle+" "+"Programdan Çıkış Yaptınız.");
+		System.out.println(lessonId+" Numaralı "+lessonTitle+" "+"Programdan Çıkış Yaptınız.");
 	}
 	
 	
