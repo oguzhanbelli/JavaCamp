a55815ba20015418e5a8ebe3411a71ceba143adf
Oğuzhan Belli <75687376+oguzhanbelli@users.noreply.github.com>
2021-04-27 21:48:31 +0300

    Update ProductManager.java

--- classesWithAttributes/src/classesWithAttributes/ProductManager.java
+++ classesWithAttributes/src/classesWithAttributes/ProductManager.java
@@ -2,13 +2,13 @@ package classesWithAttributes;
 
 public class ProductManager {
 	public void Add(Product product) {
-		System.out.println("Ürün Eklendi "+product.getName());
+		System.out.println("ÃœrÃ¼n Eklendi "+product.getName());
 		System.out.println(product.getKod());
 		
 	}
 	
 	public void Add2(int id,String name,String desc,int stockAmount,double price) {
-		//PRODUCT'I GÖNDER ATTRÝBUTELARI DEÐÝL.!
+		//PRODUCT'I GÃ–NDER ATTRIBUTELARI DEÄžÄ°L.!
 		
 		
 	}
