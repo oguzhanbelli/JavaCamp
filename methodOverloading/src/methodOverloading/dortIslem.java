2556609ab1d2dcef2b4460c6b2a166e3aba9bd5e
Oğuzhan Belli <75687376+oguzhanbelli@users.noreply.github.com>
2021-04-27 21:49:19 +0300

    Update dortIslem.java

--- methodOverloading/src/methodOverloading/dortIslem.java
+++ methodOverloading/src/methodOverloading/dortIslem.java
@@ -5,7 +5,7 @@ public class dortIslem {
 		return sayi1 + sayi2;
 
 	}
-	//METHOD OVERLOAD›NG ›NT ›NT ›NT parametrelere gˆre ezer.
+	//METHOD OVERLOAD√ùNG INT INT INT parametrelere g√∂re ezer.
 	public int topla(int sayi1,int sayi2,int sayi3) {
 		return sayi1+sayi2+sayi3;
 		
