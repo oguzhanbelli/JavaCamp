425816092ff08b2ae21bee96dd7fa9a8192ae98b
Oğuzhan Belli <75687376+oguzhanbelli@users.noreply.github.com>
2021-04-27 21:16:06 +0300

    create

---
+++ videolar1/kodlamaioOdev/src/kodlamaioOdev/Lesson.java
@@ -0,0 +1,22 @@
+package kodlamaioOdev;
+
+public class Lesson {
+	public Lesson() {
+
+	}
+	public Lesson(int id, String title, String desc, String instructor, String price) {
+		this.id = id;
+		this.title = title;
+		this.desc = desc;
+		this.instructor = instructor;
+		this.price = price;
+
+	}
+
+	int id;
+	String title;
+	String desc;
+	String instructor;
+	String price;
+
+}
