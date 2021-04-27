67e9d4646f52290b8ca91256a92e25d6e6eb9f0f
Oğuzhan Belli <75687376+oguzhanbelli@users.noreply.github.com>
2021-04-27 21:56:56 +0300

    Update Main.java

--- videolar1/multiDimentionalArrayDemo/src/multiDimentionalArrayDemo/Main.java
+++ videolar1/multiDimentionalArrayDemo/src/multiDimentionalArrayDemo/Main.java
@@ -3,15 +3,15 @@ package multiDimentionalArrayDemo;
 public class Main {
 	public static void main(String[] args) {
 		String[][] sehirler = new String[3][3];
-		sehirler[0][0] = "Ýstanbul";
+		sehirler[0][0] = "Ä°stanbul";
 		sehirler[0][1] = "Bursa";
 		sehirler[0][2] = "Bilecik";
 		sehirler[1][0] = "Ankara";
 		sehirler[1][1] = "Yozgat";
 		sehirler[1][2] = "Konya";
 		sehirler[2][0] = "Van";
-		sehirler[2][1] = "Muþ";
-		sehirler[2][2] = "Diyarbakýr";
+		sehirler[2][1] = "MuÅŸ";
+		sehirler[2][2] = "DiyarbakÄ±r";
 		
 		for(int i = 0; i <=2 ;i++) {
 			System.out.println("-----------------------");
